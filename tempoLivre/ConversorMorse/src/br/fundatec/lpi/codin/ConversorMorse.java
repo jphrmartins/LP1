965d067b154aa812c7e35758626b4489dce958c6
Tecnico - TI <tecnico@epfserver.com.br>
2017-08-08 20:12:20 -0300

    Pasta com códigos feito em tempo livre

--- tempoLivre/ConversorMorse/src/br/fundatec/lpi/codin/ConversorMorse.java
+++ tempoLivre/ConversorMorse/src/br/fundatec/lpi/codin/ConversorMorse.java
@@ -8,19 +8,133 @@ public class ConversorMorse {
 		this.mensagem = mensagem;
 		converterMensagem();
 	}
-//char letter = 'mensagem'.charAt(loop);
-	private void converterMensagem() {
-		String mensagemConvertida = "";
-		for(int loop = 0; loop <= mensagem.length(); loop++){
-			switch (mensagem.charAt(loop)) {
-			case "A":
-				
-				break;
 
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	// char letter = 'mensagem'.charAt(loop);
+	private void converterMensagem() {
+		this.mensagem = this.mensagem.toUpperCase();
+		String mensagemConvertida = "";
+		for(int loop = 0; loop < this.mensagem.length(); loop++){
+			char letter = this.mensagem.charAt(loop);
+			switch (letter) {
+			case 'A':
+				mensagemConvertida += ".-/";
+				break;
+			case 'B':
+				mensagemConvertida += "-.../";
+				break;
+			case 'C':
+				mensagemConvertida += "-.-./";
+				break;
+			case 'D':
+				mensagemConvertida += "-../";
+				break;
+			case 'E':
+				mensagemConvertida += "./";
+				break;
+			case 'F':
+				mensagemConvertida += "..-./";
+				break;
+			case 'G':
+				mensagemConvertida += "--./";
+				break;
+			case 'H':
+				mensagemConvertida += "..../";
+				break;
+			case 'I':
+				mensagemConvertida += "../";
+				break;
+			case 'J':
+				mensagemConvertida += ".---/";
+				break;
+			case 'K':
+				mensagemConvertida += "-.-/";
+				break;
+			case 'L':
+				mensagemConvertida += ".-../";
+				break;
+			case 'M':
+				mensagemConvertida += "--/";
+				break;
+			case 'N':
+				mensagemConvertida += "-./";
+				break;
+			case 'O':
+				mensagemConvertida += "---/";
+				break;
+			case 'P':
+				mensagemConvertida += ".--./";
+				break;
+			case 'Q':
+				mensagemConvertida += "--.-/";
+				break;
+			case 'R':
+				mensagemConvertida += ".-./";
+				break;
+			case 'S':
+				mensagemConvertida += ".../";
+				break;
+			case 'T':
+				mensagemConvertida += "-/";
+				break;
+			case 'U':
+				mensagemConvertida += "..-/";
+				break;
+			case 'V':
+				mensagemConvertida += "...-/";
+				break;
+			case 'W':
+				mensagemConvertida += ".--/";
+				break;
+			case 'X':
+				mensagemConvertida += "-..-/";
+				break;
+			case 'Y':
+				mensagemConvertida += "-.--/";
+				break;
+			case 'Z':
+				mensagemConvertida += "--../";
+				break;
+			case '1':
+				mensagemConvertida += ".----/";
+				break;
+			case '2':
+				mensagemConvertida += "..---/";
+				break;
+			case '3':
+				mensagemConvertida += "...--/";
+				break;
+			case '4':
+				mensagemConvertida += "....-/";
+				break;
+			case '5':
+				mensagemConvertida += "...../";
+				break;
+			case '6':
+				mensagemConvertida += "-..../";
+				break;
+			case '7':
+				mensagemConvertida += "--.../";
+				break;
+			case '8':
+				mensagemConvertida += "---../";
+				break;
+			case '9':
+				mensagemConvertida += "----./";
+				break;
+			case '0':
+				mensagemConvertida += "-----/";
+				break;
 			default:
+				mensagemConvertida += " /";
 				break;
 			}
+			
 		}
+		this.mensagem = mensagemConvertida;
 	}
-	
+
 }
--- tempoLivre/ConversorMorse/src/br/fundatec/lpi/codin/app/ConversorApp.java
+++ tempoLivre/ConversorMorse/src/br/fundatec/lpi/codin/app/ConversorApp.java
@@ -4,7 +4,8 @@ import br.fundatec.lpi.codin.ConversorMorse;
 
 public class ConversorApp {
 	public static void main(String[] args) {
-		ConversorMorse conv = new ConversorMorse();
+		ConversorMorse conv = new ConversorMorse("Abacaxi 123");
+		System.out.println(conv.getMensagem());
 	}
 
 }
